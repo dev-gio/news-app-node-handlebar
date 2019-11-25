187e2054235882c312eb8d6d8bc226075a057c8d
dev-gio <ghio.cabang@gmail.com>
2019-11-25 18:02:09 +0800

    Code formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => { 
+document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.news-wrapper').forEach(link => {
-        link.addEventListener('click', function(event) {
+        link.addEventListener('click', function (event) {
             let url = link.getAttribute("data-url");
             window.open(url, '_blank');
         });
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 var path = require("path");
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 const fs = require('fs');
 
 const app = express();
 
 //view engine setup
 app.set("views", path.join(__dirname, "views")); //setting views directory for views.
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set("view engine", "handlebars"); //setting view engine as handlebars
 
 app.set('port', (process.env.PORT || 3000));
@@ -15,29 +15,11 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
-    // fetch('./news-collection.json')
-    // .then((res) => res.json())
-    // .then((data) => {
-    //     // data.forEach((question, i) => {
-            
-    //     // });
-        
-	// res.render('home', {data: data});
-    // });
-
-    // axios.get('./news-collection.json')
-    //     .then((res) => {
-    //         res.render('home', {data: data});
-    //     }).catch((err) => {
-    //         res.send(err);
-    //     });
-
     let rawdata = fs.readFileSync('news-collection.json');
     let news = JSON.parse(rawdata);
-    res.render('home', {data: news});
-    // res.send(news);
+    res.render('home', { data: news });
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log(`Server started on port ${app.get('port')}`);
 });
