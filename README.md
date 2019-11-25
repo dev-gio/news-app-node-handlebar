78ab41229b80052f4b9097ab7ae9111500966d2c
dev-gio <ghio.cabang@gmail.com>
2019-11-25 17:58:22 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # news-app
  Practice Project: News App - Node JS
+
+Goal: This practice project is for the sole purpose of learning Node Js and Handlebars
---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => { 
+    document.querySelectorAll('.news-wrapper').forEach(link => {
+        link.addEventListener('click', function(event) {
+            let url = link.getAttribute("data-url");
+            window.open(url, '_blank');
+        });
+    });
+});
